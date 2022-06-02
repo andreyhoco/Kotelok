9c54cba826e0d44c1d36af42f0ae2be8230ae220
andreyhoco <andreygud2013@gmail.com>
2022-06-02 18:12:03 +0300

    Добавил тренажер TrainerCards

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/trainer/TrainerCards.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/trainer/TrainerCards.kt
@@ -2,24 +2,64 @@ package com.designdrivendevelopment.kotelok.trainer
 
 import com.designdrivendevelopment.kotelok.entities.LearnableDefinition
 import com.designdrivendevelopment.kotelok.screens.trainers.LearnableDefinitionsRepository
-
-const val CARDS_WEIGHT = 0.4f
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 
 class TrainerCards(
     learnableDefinitionsRepository: LearnableDefinitionsRepository,
-    changeStatisticsRepository: ChangeStatisticsRepository,
-) :
-    IteratorTrainerSingle<Boolean>(
-        learnableDefinitionsRepository,
-        changeStatisticsRepository,
-        CARDS_WEIGHT
-    ) {
+    private val changeStatisticsRepository: ChangeStatisticsRepository,
+    dispatcher: CoroutineDispatcher = Dispatchers.IO,
+) : ReactiveTrainer<Boolean, List<LearnableDefinition>>(
+    learnableDefinitionsRepository,
+    TRAINER_CARDS_WEIGHT,
+    dispatcher
+) {
+    private val repeatListMutex = Mutex()
+    private val repeatList = mutableListOf<LearnableDefinition>()
+    private val definitionsFlow = MutableSharedFlow<List<LearnableDefinition>>(
+        replay = 1,
+        extraBufferCapacity = 0,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    override val outputFlow: SharedFlow<List<LearnableDefinition>>
+        get() = definitionsFlow.asSharedFlow()
 
-    override fun rateEF(expectedWord: LearnableDefinition, userInput: Boolean): Int {
-        return if (userInput) {
-            LearnableDefinition.GRADE_FOUR
-        } else {
-            0
+    override suspend fun onLoadDefinitions() = withContext(dispatcher) {
+        repeatListMutex.withLock { repeatList.clear() }
+        definitionsFlow.emit(loadedDefinitions)
+    }
+
+    override suspend fun onAnswerRight(definition: LearnableDefinition) = withContext(dispatcher) {
+        changeStatisticsRepository.addSuccessfulResultToWordDef(definition.definitionId)
+    }
+
+    override suspend fun onAnswerWrong(definition: LearnableDefinition) = withContext(dispatcher) {
+        changeStatisticsRepository.addFailedResultToWordDef(definition.definitionId)
+        repeatListMutex.withLock {
+            repeatList.add(definition)
+            repeatList.shuffle()
         }
+    }
+
+    override fun rateEF(userAnswer: UserAnswer<Boolean>): Int {
+        return if (userAnswer.answer) LearnableDefinition.GRADE_FOUR else LearnableDefinition.GRADE_ZERO
+    }
+
+    suspend fun updateDefinitionsFlow(
+        remainingDefinitions: List<LearnableDefinition> = emptyList()
+    ) = withContext(dispatcher) {
+        definitionsFlow.emit(remainingDefinitions + repeatList.toList())
+        repeatListMutex.withLock { repeatList.clear() }
+    }
+
+    companion object {
+        const val TRAINER_CARDS_WEIGHT = 0.4f
     }
 }
