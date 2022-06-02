870a37a744ab3e3d1100ab79f898e9bff404a62f
andreyhoco <andreygud2013@gmail.com>
2022-06-02 18:12:31 +0300

    Добавил тренажер TrainerWriter

--- app/src/main/kotlin/com/designdrivendevelopment/kotelok/trainer/TrainerWriter.kt
+++ app/src/main/kotlin/com/designdrivendevelopment/kotelok/trainer/TrainerWriter.kt
@@ -4,19 +4,23 @@ import com.designdrivendevelopment.kotelok.entities.LearnableDefinition
 import com.designdrivendevelopment.kotelok.screens.trainers.LearnableDefinitionsRepository
 import com.designdrivendevelopment.kotelok.trainer.utils.WordChangeArray
 import com.designdrivendevelopment.kotelok.trainer.utils.levenshteinDifference
+import java.util.LinkedList
+import kotlin.NoSuchElementException
 import kotlin.math.roundToInt
-
-const val WRITE_WEIGHT = 1f
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class TrainerWriter(
     learnableDefinitionsRepository: LearnableDefinitionsRepository,
-    changeStatisticsRepository: ChangeStatisticsRepository,
-) :
-    IteratorTrainerSingle<String>(
-        learnableDefinitionsRepository,
-        changeStatisticsRepository,
-        WRITE_WEIGHT
-    ) {
+    private val changeStatisticsRepository: ChangeStatisticsRepository,
+    dispatcher: CoroutineDispatcher = Dispatchers.IO
+) : IteratorTrainer<String, LearnableDefinition>(
+    learnableDefinitionsRepository,
+    TRAINER_WRITER_WEIGHT,
+    dispatcher
+) {
+    private val repeatList: LinkedList<LearnableDefinition> = LinkedList<LearnableDefinition>()
 
     /*
     expectedStr: cats
@@ -26,17 +30,44 @@ class TrainerWriter(
     var curWordChange: WordChangeArray = emptyArray()
         private set
 
-    override fun rateEF(expectedWord: LearnableDefinition, userInput: String): Int {
+    override fun hasNext(): Boolean = repeatList.isNotEmpty()
+
+    override fun next(): LearnableDefinition {
+        if (hasNext()) {
+            return repeatList.removeFirst()
+        } else throw NoSuchElementException("RepeatList is empty")
+    }
+
+    override suspend fun onLoadDefinitions(): Unit = withContext(dispatcher) {
+        repeatList.clear()
+        repeatList.addAll(loadedDefinitions)
+    }
+
+    override suspend fun onAnswerRight(definition: LearnableDefinition) = withContext(dispatcher) {
+        changeStatisticsRepository.addSuccessfulResultToWordDef(definition.definitionId)
+    }
+
+    override suspend fun onAnswerWrong(definition: LearnableDefinition) = withContext(dispatcher) {
+        changeStatisticsRepository.addFailedResultToWordDef(definition.definitionId)
+        repeatList.addFirst(definition)
+        repeatList.shuffle()
+    }
+
+    override fun rateEF(userAnswer: UserAnswer<String>): Int {
         val (levenshteinDistance, path) = levenshteinDifference(
-            expectedStr = expectedWord.writing,
-            userStr = userInput
+            expectedStr = userAnswer.definition.writing,
+            userStr = userAnswer.answer
         )
         curWordChange = path
         // levenshteinDistance ∈ [0, max(expectedStr.length, userStr.length)]
-        val errorRate = levenshteinDistance.toDouble() / expectedWord.writing.length
+        val errorRate = levenshteinDistance.toDouble() / userAnswer.definition.writing.length
         val correctRate = (1 - minOf(1.0, errorRate)) // ∈ [0, 1]
         val normalizedCorrectRate = correctRate * (LearnableDefinition.GRADE_ARRAY.size - 1)
 
         return LearnableDefinition.GRADE_ARRAY[normalizedCorrectRate.roundToInt()]
+    }
+
+    companion object {
+        const val TRAINER_WRITER_WEIGHT = 1f
     }
 }
